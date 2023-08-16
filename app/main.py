d9034aeacf192f68b78d9dd45e86289e3738f5db
Dillon O'Leary <doleary@wisc.edu>
2023-08-16 01:00:42 -0700

    Pass tests for registering a donation

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
+import datetime
+from pydantic import BaseModel
 import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class BaseDonation(BaseModel):
+    donor_name: str
+    donation_type: str
+    units_donated: str
+    donation_date: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, World"}
+class DonationWithID(BaseDonation):
+    timestamp: str
+
+@app.post("/")
+async def register_donation(donation: BaseDonation) -> DonationWithID:
+    return {
+        "donor_name": "phil",
+        "donation_type": "money",
+        "units_donated": "5",
+        "donation_date": "2008-09-15",
+        "timestamp": "12345"
+    }
 
 # for debugging purposes
 if __name__ == "__main__":
--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,32 @@ from .main import app
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_post_donation():
+    response = client.post(
+        "/",
+        json={
+            "donor_name": "phil",
+            "donation_type": "money",
+            "units_donated": "5",
+            "donation_date": "2008-09-15"
+        }
+    )
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, World"}
+    assert response.json() == {
+        "donor_name": "phil",
+        "donation_type": "money",
+        "units_donated": "5",
+        "donation_date": "2008-09-15",
+        "timestamp": "12345"
+    }
+
+def test_post_bad_donation():
+    response = client.post(
+        "/",
+        json={
+            "donor_name": "phil",
+            "units_donated": "5",
+            "donation_date": "2008-09-15"
+        }
+    )
+    assert response.status_code == 422

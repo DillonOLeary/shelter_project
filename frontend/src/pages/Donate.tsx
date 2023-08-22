ad71de0eb19336e2eda6e631b59ba0e3e8deb2fa
Dillon O'Leary <doleary@wisc.edu>
2023-08-22 15:59:51 -0700

    Add onSubmit to Donate

--- frontend/src/pages/Donate.tsx
+++ frontend/src/pages/Donate.tsx
@@ -8,48 +8,51 @@ const Donate: FC<any> = (): ReactElement => {
         unitsDonated: "",
         donationDate: ""
     });
-    function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
         setInputs({ ...inputs, [name]: value });
     }
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(inputs);
+    }
     return (
-        <form>
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <TextField
-                    value={inputs.donorName}
-                    name="donorName"
-                    label="Donor Name"
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    value={inputs.donationType}
-                    name="donationType"
-                    label="Donation Type"
-                    onChange={handleInputChange}
-                />
-                <TextField
-                    value={inputs.unitsDonated}
-                    name="unitsDonated"
-                    label="Units Donated"
-                    onChange={handleInputChange}
-                    type={"number"}
-                />
-                <TextField
-                    value={inputs.donationDate}
-                    name="donationDate"
-                    label="Donation Date"
-                    onChange={handleInputChange}
-                />
-                <Button sx={{ m: 1 }} type="submit" variant="outlined">Submit</Button>
-            </Box>
-        </form>
+        <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+        >
+            <TextField
+                value={inputs.donorName}
+                name="donorName"
+                label="Donor Name"
+                onChange={handleInputChange}
+            />
+            <TextField
+                value={inputs.donationType}
+                name="donationType"
+                label="Donation Type"
+                onChange={handleInputChange}
+            />
+            <TextField
+                value={inputs.unitsDonated}
+                name="unitsDonated"
+                label="Units Donated"
+                onChange={handleInputChange}
+                type={"number"}
+            />
+            <TextField
+                value={inputs.donationDate}
+                name="donationDate"
+                label="Donation Date"
+                onChange={handleInputChange}
+            />
+            <Button sx={{ m: 1 }} type="submit" variant="outlined">Submit</Button>
+        </Box>
     );
 };
 
